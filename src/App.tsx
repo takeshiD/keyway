2dd22fce8a66883f0f29aad9cbd54981f9ebec4f
takeshiD <goriponikeike55@gmail.com>
2024-08-16 00:06:50 +0900

    change from pure-rust to tauri

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <button className='btn btn-primary'>Button</button>
+    </>
+  )
+}
+
+export default App
