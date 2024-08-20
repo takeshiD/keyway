b100882c8a19544b9a43fcf50049a6a68cdd10fd
takeshiD <goriponikeike55@gmail.com>
2024-08-20 13:24:01 +0900

    update

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+
 fn main() {
-    let app = tauri::Builder::default()
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+    let system_tray = SystemTray::new().with_menu(tray_menu);
+    tauri::Builder::default()
+        .system_tray(system_tray)
         .build(tauri::generate_context!())
-        .expect("Error while building tauri application");
-    app.run(|_, _| {});
+        .expect("Error while building tauri application")
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
